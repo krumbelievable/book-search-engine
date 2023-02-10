d0bdb207795a800c0ab42f05b3e0a0faca79c474
Krumbelievable <krumbach.will@gmail.com>
2023-02-10 17:14:11 -0600

    organized some code

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,21 @@
-import { gql } from '@apollo/client'
+import { gql } from '@apollo/client';
 
-export const GET_ME = gql `
-{
-    me {
-        _id
-        username
-        email
-        bookCount
-        savedBooks {
-            bookId
-            authors
-            description
-            title
-            image
-            link
-        }
-    }
-}`;
+// queries refelct server side
+export const GET_ME = gql`
+	{
+		me {
+			_id
+			username
+			email
+			bookCount
+			savedBooks {
+				bookId
+				authors
+				description
+				title
+				image
+				link
+			}
+		}
+	}
+`;
